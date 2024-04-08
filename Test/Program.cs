406c5ff1bc68dd05eecd0659736bc249d9557bf8
Tiitmouse <lkovacic380@gmail.com>
2024-04-08 20:31:02 +0200

    Unversioned files

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using QuickType;
+using WCup_Data.DataFetching;
+using WCup_Data.Models;
+using WCup_Data.Settings;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Settings settings = SettingsController.GetSettings();
+            Console.WriteLine("loaded settings");
+            IDataFetcher fetcher = FetchFactory.FetchData(settings.DataFetchType);
+            List<Match> matches = await fetcher.FetchMatches();
+            List<Group> groups = await fetcher.FetchGroups();
+            List<Result> results = await fetcher.FetchResults();
+            List<TeamDetails> teams = await fetcher.FetchTeamDetails();
+            SettingsController.SaveSettings();
+            Console.WriteLine("saved settings");
+        }
+    }  
+}
