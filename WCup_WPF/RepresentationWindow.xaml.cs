6d725f4e7ec76c819ccd28e65ad47e34af6ceef1
lorena <lkovacic380@gmail.com>
2024-09-09 03:10:12 -0700

    wpf settings template

---
+++ WCup_WPF/RepresentationWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WCup_WPF
+{
+    /// <summary>
+    /// Interaction logic for RepresentationWindow.xaml
+    /// </summary>
+    public partial class RepresentationWindow : Window
+    {
+        public RepresentationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
